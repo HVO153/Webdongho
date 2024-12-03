5910df0e1f33c72547de0b8e68da9482c8793e40
HunngLee2k3 <145043192+HunngLee2k3@users.noreply.github.com>
2024-12-03 14:45:12 +0700

    Chinh sua mot it cho layout nha

--- WebDongHo/ViewModels/HomeViewModel.cs
+++ WebDongHo/ViewModels/HomeViewModel.cs
@@ -7,9 +7,11 @@ namespace WebDongHo.ViewModels
         public List<Menu> Menus { get; set; }
         public List<Blog> Blogs { get; set; }
         public List<Slider> Sliders { get; set; }
+
         public List<Product> MecWatchProds { get; set; }
         public List<Product> SmaWatchProds { get; set; }
         public List<Product> PinWatchProds { get; set; }
+
         public Catology MecWCateProds { get; set; }
         public Catology SmaWCateProds { get; set; }
         public Catology PinWCateProds { get; set; }
